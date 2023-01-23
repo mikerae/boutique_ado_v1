130a7a5efd1edde7d6a066b2dadc446505927ea2
Mike Rae <mikerae@me.com>
2023-01-23 19:11:09 +0000

    Match line 16 settings.py with source code

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
